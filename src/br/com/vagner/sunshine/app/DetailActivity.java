12e1d57994732833ab8199a2a6489f17518f8f91
vagner.oliveirars <vagner.oliveirars@gmail.com>
2015-02-06 02:31:34 +0000

    imports fixed.

--- src/br/com/vagner/sunshine/app/DetailActivity.java
+++ src/br/com/vagner/sunshine/app/DetailActivity.java
@@ -1,17 +1,15 @@
 package br.com.vagner.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.os.Build;
 
 public class DetailActivity extends ActionBarActivity {
 
